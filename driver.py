2345455a43e81c678013463eff20295f2e4df204
Adithya-Sharma <adithyamittur.am.am@gmail.com>
2020-05-20 21:34:33 +0530

    upload scraper and run code

---
+++ driver.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 18:38:28 2020
+
+@author: Dell
+"""
+
+import glassdoor_scriper as gs
+import pandas as pd
+path="C:/Users/Dell/Documents/ds_salary_proj/chromedriver"
+
+df=gs.get_jobs('Data Scientist, Machine Learning,Deep Learning',15,False,path,15)
