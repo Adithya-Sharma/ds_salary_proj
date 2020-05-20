e38b30261f30a81be5692c7ea74391e2d37ad1f3
Adithya-Sharma <adithyamittur.am.am@gmail.com>
2020-05-21 00:26:19 +0530

    uploading data and data collection scipt

--- driver.py
+++ driver.py
@@ -9,4 +9,5 @@ import glassdoor_scriper as gs
 import pandas as pd
 path="C:/Users/Dell/Documents/ds_salary_proj/chromedriver"
 
-df=gs.get_jobs('Data Scientist, Machine Learning,Deep Learning',15,False,path,15)
+df=gs.get_jobs('Data Scientist, Machine Learning,Deep Learning',100,False,path,15)
+df.to_csv('glassdoor_jobs.csv',index=False)
